b7c0609e9c4eee6db45a298a552775b7439c6180
MarcosPaul0 <marcosphip7@gmail.com>
2023-04-16 13:45:59 -0300

    feat: configuração do jest

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,9 @@
+import App from '@app/index';
+import renderer from 'react-test-renderer';
+
+describe('<Text />', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
