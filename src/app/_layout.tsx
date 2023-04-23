34b925737a78a7d7f90e867a0aa782cd0e3a2279
MarcosPaul0 <marcosphip7@gmail.com>
2023-04-22 22:10:06 -0300

    feat: adiciona navegação em stack e tabs

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router/stack';
+
+export default function TabsLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
