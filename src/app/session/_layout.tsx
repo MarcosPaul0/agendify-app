7022057b62b61e066e153f40ef5ccdf8e175603c
MarcosPaul0 <marcosphip7@gmail.com>
2023-04-22 22:10:16 -0300

    feat: adiciona navegação em stack e tabs

---
+++ src/app/session/_layout.tsx
@@ -0,0 +1,73 @@
+import { APP_ROUTES } from '@constants/appRoutes.constant';
+import { COLORS } from '@constants/colors.constant';
+import { useRouter, usePathname } from 'expo-router';
+import { Tabs } from 'expo-router/tabs';
+import { Briefcase, ListDashes, Plus } from 'phosphor-react-native';
+import { TouchableOpacity } from 'react-native';
+
+export default function TabsLayout() {
+  const router = useRouter();
+
+  const path = usePathname();
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.BLUE_100,
+        },
+      }}
+    >
+      <Tabs.Screen
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => (
+            <ListDashes
+              size={32}
+              color={focused ? COLORS.BLUE_600 : COLORS.BLUE_800}
+            />
+          ),
+        }}
+        name="searchServices"
+      />
+
+      <Tabs.Screen
+        options={{
+          tabBarShowLabel: false,
+          tabBarButton: ({ to }) => {
+            if (path === APP_ROUTES.MY_SERVICES) {
+              return (
+                <TouchableOpacity
+                  className={`
+                    p-3 rounded-full bg-BLUE_500
+                    absolute bottom-4 left-40
+                  `}
+                  onPress={() => router.push(to as string)}
+                >
+                  <Plus size={40} color={COLORS.GRAY_50} />
+                </TouchableOpacity>
+              );
+            }
+
+            return null;
+          },
+        }}
+        name="registerBusiness"
+      />
+
+      <Tabs.Screen
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => (
+            <Briefcase
+              size={32}
+              color={focused ? COLORS.BLUE_600 : COLORS.BLUE_800}
+            />
+          ),
+        }}
+        name="myServices"
+      />
+    </Tabs>
+  );
+}
