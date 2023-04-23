d352f9c7132184fcd96282cce5f3e6a0aa2923a8
MarcosPaul0 <marcosphip7@gmail.com>
2023-04-22 22:15:34 -0300

    feat: implementa componente de input de busca

---
+++ src/components/SearchInput/index.tsx
@@ -0,0 +1,30 @@
+import { MagnifyingGlass } from 'phosphor-react-native';
+import { View, TextInput } from 'react-native';
+import { COLORS } from '@constants/colors.constant';
+import { ISearchInputProps } from './interfaces/searchInputProps.interface';
+
+export function SearchInput({
+  search,
+  changeSearch,
+  ...rest
+}: ISearchInputProps) {
+  return (
+    <View
+      className={`
+        w-full h-11 pl-2.5 mb-4 flex-row items-center
+        bg-GRAY_100 border border-GRAY_500 rounded-lg
+      `}
+    >
+      <MagnifyingGlass size={24} color={COLORS.GRAY_700} weight="bold" />
+      <TextInput
+        className={`
+          text-base text-GRAY_800 w-full h-11 pl-2.5 
+        `}
+        value={search}
+        onChangeText={changeSearch}
+        selectionColor={COLORS.GRAY_800}
+        {...rest}
+      />
+    </View>
+  );
+}
