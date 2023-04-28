27fa69d6cf29fd9b5ac1d08dd42406df3bccaf98
MarcosPaul0 <marcosphip7@gmail.com>
2023-04-28 20:17:26 -0300

    feat: adiciona de layout de tab navigation no contexto do negócio

---
+++ src/app/session/(business)/_layout.tsx
@@ -0,0 +1,63 @@
+import { HeaderMenu } from '@components/HeaderMenu';
+import { COLORS } from '@constants/colors.constant';
+import { Tabs } from 'expo-router/tabs';
+import { Calendar, House } from 'phosphor-react-native';
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: COLORS.BLUE_100,
+        },
+      }}
+    >
+      <Tabs.Screen
+        options={{
+          headerTitle: () => null,
+          tabBarShowLabel: false,
+          headerStyle: {
+            backgroundColor: COLORS.BLUE_100,
+          },
+          headerRight: () => <HeaderMenu />,
+          tabBarIcon: ({ focused }) => (
+            <House
+              size={32}
+              color={focused ? COLORS.BLUE_600 : COLORS.BLUE_800}
+            />
+          ),
+        }}
+        name="home"
+      />
+
+      <Tabs.Screen
+        options={{
+          headerTitle: () => null,
+          tabBarShowLabel: false,
+          headerStyle: {
+            backgroundColor: COLORS.BLUE_100,
+          },
+          tabBarIcon: ({ focused }) => (
+            <Calendar
+              size={32}
+              color={focused ? COLORS.BLUE_600 : COLORS.BLUE_800}
+            />
+          ),
+        }}
+        name="myCalendar"
+      />
+
+      <Tabs.Screen
+        options={{
+          headerTitle: 'Dados do meu negócio',
+          tabBarShowLabel: false,
+          headerStyle: {
+            backgroundColor: COLORS.BLUE_100,
+          },
+          tabBarButton: () => null,
+        }}
+        name="manageBusiness"
+      />
+    </Tabs>
+  );
+}
