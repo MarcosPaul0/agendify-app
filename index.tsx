dad4ff8e506099dcd2a74287162758681ab21080
MarcosPaul0 <marcosphip7@gmail.com>
2023-05-13 10:44:48 -0300

    feat: adiciona providers

--- index.tsx
+++ index.tsx
@@ -1,11 +1,19 @@
 import 'expo-router/entry';
 import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
+import { AuthContextProvider } from '@contexts/AuthContext';
+import { ToastProvider } from 'react-native-toast-notifications';
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
   const ctx = require.context('./src/app');
-  return <ExpoRoot context={ctx} />;
+  return (
+    <ToastProvider>
+      <AuthContextProvider>
+        <ExpoRoot context={ctx} />
+      </AuthContextProvider>
+    </ToastProvider>
+  );
 }
 
 registerRootComponent(App);
